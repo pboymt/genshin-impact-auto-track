8a3a96002a044c3b9b8b9d35d3a7afaf3616278c
pboymt <10893582+pboymt@users.noreply.github.com>
2022-10-09 14:46:02 +0800

    docs: 更新类型注释，更新 typedoc

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -87,7 +87,7 @@ declare module 'genshin-impact-auto-track' {
      *
      * 某些原神数据的获取是基于原神客户端的，因此需要一个客户端的窗口句柄。默认情况下此句柄由程序自动获取，但有可能会因为编码问题或其它未知的原因而获取不到，此时就需要手动指定客户端的窗口句柄。
      *
-     * 注意，不论本函数返回何值，`handle` 只要不是 `0`，就会被设置为窗口句柄，并禁止自动获取窗口句柄。
+     * 注意，不论本函数返回何值，`hwnd` 只要不是 `0`，就会被设置为窗口句柄，并禁止自动获取窗口句柄。
      * 
      * @param {number} hwnd 原神客户端的窗口句柄。
      */
@@ -140,18 +140,32 @@ declare module 'genshin-impact-auto-track' {
     export function setWorldScale(scale: number): boolean;
 
     /**
-     * 获取所在位置的所在地图区域并重映射坐标区域。
-     *
-     * ### 参数
-     *
-     * - `x` 获取到的人物的x轴坐标位置。
-     * - `y` 获取到的人物的y轴坐标位置。
-     * - `mapId` 获取到的人物的所在地图区域。
+     * 获取当前人物所在位置以及角度（箭头朝向）。
      *
      * ### 返回值
      *
-     * - `true` 表示坐标位置获取成功。
-     * - `false` 表示坐标位置获取失败，此时三个参数均不会被改变。
+     * 返回一个对象，包含以下属性：
+     * - `x` 获取到的人物的x轴坐标位置。
+     * - `y` 获取到的人物的y轴坐标位置。
+     * - `a` 获取到的人物的角度（箭头朝向）。
+     *
+     * ### 说明
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     * 
+     * @returns {TransformOfMap} 获取到的人物的所在地图区域。
+     */
+    export function getTransformOfMap(): TransformOfMap;
+
+    /**
+     * 获取所在位置的所在地图区域并重映射坐标区域。
+     *
+     * ### 返回值
+     *
+     * 返回一个对象，包含以下属性：
+     * - `x` 获取到的人物的x轴坐标位置。
+     * - `y` 获取到的人物的y轴坐标位置。
+     * - `mapId` 获取到的人物的所在地图区域。
      *
      * ### 说明
      *
@@ -162,24 +176,131 @@ declare module 'genshin-impact-auto-track' {
      * | `0`      | 提瓦特大陆。             |
      * | `1`      | 渊下宫                   |
      * | `2`      | 地下层岩                 |
-     *
-     * 无论成功与否都会设置 `LastErr` 值。调用 [`GetLastErr()`](#GetLastErr) 可以获取错误码。
-     *
-     * 调用本函数前，如果没有初始化过，或最后一次初始化返回失败，则需要调用 [`init()`](#init) 函数进行初始化。
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
      * 
      * @returns {TransformOfMap} 获取到的人物的所在地图区域。
      */
-    export function getTransformOfMap(): TransformOfMap;
-
     export function getPositionOfMap(): PositionOfMap;
+
+    /**
+     * 获取当前角度（箭头朝向）。
+     * 
+     * @returns {number} 获取到的人物的角度（箭头朝向）。
+     * 
+     * ### 说明
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     */
     export function getDirection(): number;
+
+    /**
+     * 获取当前视角方位（视角朝向）。
+     * 
+     * ### 说明
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     * 
+     * @returns {number} 获取到的人物的视角方位（视角朝向）。
+     */
     export function getRotation(): number;
+
     export function getStar(): Star;
     export function getStarJson(): string;
+
+    /**
+     * 获取在屏幕右下角显示的玩家的UID。
+     * 
+     * ### 说明
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     * 
+     * @returns {number} 获取到的玩家的UID。
+     */
     export function getUID(): number;
+
+    /**
+     * 获取本地图片中的UID、当前人物所在位置以及角度（箭头朝向）。
+     * 
+     * ### 参数
+     * 
+     * - `path` 输入图片文件路径
+     * 
+     * ### 返回值
+     * 
+     * 返回一个对象，包含以下属性：
+     * - `uid` 获取到的玩家的UID。
+     * - `x` 获取到的人物的x轴坐标位置。
+     * - `y` 获取到的人物的y轴坐标位置。
+     * - `a` 获取到的人物的角度（箭头朝向）。
+     * 
+     * ### 说明
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     * 
+     * @param path 输入图片文件路径
+     */
     export function getInfoLoadPicture(path: string): Info;
+
+    /**
+     * 获取本地视频中的UID、当前人物所在位置以及角度（箭头朝向）并保存至文本中。
+     * 
+     * ### 参数
+     * - `path` 输入视频文件路径
+     * - `pathOutFile` 输出的路径数据保存文件路径
+     * 
+     * ### 返回值
+     * 
+     * 返回一个对象，包含以下属性：
+     * - `path` 输入视频文件路径
+     * - `pathOutFile` 输出的路径数据保存文件路径
+     * 
+     * ### 说明
+     * 
+     * 写入文本格式如下：
+     * ```
+     * | int Time | int uid | double x | double y | double a | 
+     * ```
+     * 
+     * **注意**：暂未对错误进行处理，如果获取失败，可能会报错。
+     * 
+     * @param path 
+     * @param pathOutFile 
+     */
     export function getInfoLoadVideo(path: string, pathOutFile: string): InfoLoadVideo;
+
     export function debugCapture(): boolean;
+
+    /**
+     * 获取最后设置的错误码。
+     * 
+     * ### 返回值
+     * 
+     * 返回一个整数，表示错误码。错误码含义如下：
+     * 
+     * | 错误码 | 含义                                     | 可能的原因 |
+     * | ------ | ---------------------------------------- | ---------- |
+     * | `0`    | 执行成功/正常退出。                      |            |
+     * | `1`    | 未初始化（当前处在 **未初始化** 状态）。 |            |
+     * | `2`    | 未能找到原神窗口句柄。                   |            |
+     * | `3`    | 窗口画面为空。                           |            |
+     * | `4`    | 未能匹配到特征点。                       |            |
+     * | `5`    | 原神小地图区域为空或者区域长宽小于60px。 |            |
+     * | `6`    | 未能匹配到派蒙。                         |            |
+     * | `7`    | 特征点数组访问越界，是个bug。            |            |
+     * | `8`    | 未能在UID区域检测到有效UID。             |            |
+     * | `9`    | 提取小箭头特征误差过大。                 |            |
+     * | `10`   | 无效句柄或指定句柄所指向窗口不存在。     |            |
+     * | `11`   | 未能取到小箭头区域。                     |            |
+     * | `12`   | 窗口句柄失效。                           |            |
+     * | `13`   | 窗口大小小于480px * 360px。              |            |
+     * ### 说明
+     *
+     * 由于函数成功执行后可能不会将错误码设为 `0` ，以及一些函数不会设置错误码，因此取出的错误码不一定代表前一条函数产生/引发的错误。
+     * 
+     * @returns {number} 获取到的错误码。
+     */
     export function getLastErr(): number;
+
     export function getLastErrMsg(): string;
 }
