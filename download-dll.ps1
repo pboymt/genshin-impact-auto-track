9c97148d616376f79f37327d0185ee03444b4bba
pboymt <10893582+pboymt@users.noreply.github.com>
2022-10-10 23:08:58 +0800

    build: 修改下载脚本执行中的输出内容

--- download-dll.ps1
+++ download-dll.ps1
@@ -7,12 +7,16 @@ if (Test-Path -Path ".\cvAutoTrack") {
 
 $latest_release = Invoke-WebRequest "https://api.github.com/repos/GengGode/GenshinImpact_AutoTrack_DLL/releases/latest"
 $latest_release_url = $latest_release.content | ConvertFrom-Json | Select-Object -ExpandProperty assets | Select-Object -ExpandProperty browser_download_url
-Write-Output "Downloading latest 'GenshinImpact_AutoTrack_DLL' release from $latest_release_url"
+
 # Select 7z file
 $latest_release_url = $latest_release_url | Where-Object { $_ -like "*7z" }
+Write-Output "Downloading latest 'GenshinImpact_AutoTrack_DLL' release from $latest_release_url"
+
 # Download 7z file
 Invoke-WebRequest -Uri $latest_release_url -OutFile "cvAutoTrack.7z"
+
 # Extract 7z file to cvAutoTrack
 7z x cvAutoTrack.7z -o"cvAutoTrack" -y
+
 # Remove 7z file
 Remove-Item cvAutoTrack.7z
