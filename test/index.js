895020f50d0107ed41c91e8cf43253164ad87096
pboymt <10893582+pboymt@users.noreply.github.com>
2022-10-10 23:17:43 +0800

    test: 更新测试代码

--- test/index.js
+++ test/index.js
@@ -1,3 +1,37 @@
+const { setTimeout } = require('timers/promises');
 const addon = require('..');
 
 console.log(addon.version());
+
+process.on('beforeExit', () => {
+    addon.uninit();
+});
+
+(async () => {
+
+    const inited = addon.init();
+
+    if (!inited) {
+        console.log('Failed to init addon');
+        return;
+    }
+
+    addon.setUseDx11CaptureMode();
+
+    await setTimeout(3000);
+
+    while (true) {
+        const star = addon.getDirection();
+        console.log(star);
+    }
+
+})().catch((err) => {
+    console.error(err);
+    console.log(addon.getLastErr());
+    console.log(addon.getLastErrMsg());
+
+    addon.debugCapture();
+
+}).finally(() => {
+    addon.uninit();
+});
